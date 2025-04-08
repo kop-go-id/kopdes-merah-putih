154ad485e9c287eeef59e0e56228361747a568b5
Zain <masterzera111@gmail.com>
2025-04-09 05:45:57 +0700

    [FEAT] provinces,districts,subdistricts, and villages

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\LocationController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -12,3 +13,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/provinces', [LocationController::class, 'provinces']);
+Route::get('/districts/by-province-code/{provinceCode}', [LocationController::class, 'districts']);
+Route::get('/sub-districts/by-district-code/{districtCode}', [LocationController::class, 'subDistricts']);
+Route::get('/villages/by-sub-district-code/{subDistrictCode}', [LocationController::class, 'villages']);
