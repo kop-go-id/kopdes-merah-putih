0f36a5a8e1f79020d92ba7a0b149d0ae23bf75fe
Emil Maulana <emilmaulana10gmail.com>
2025-04-08 14:08:30 +0700

    add NPAK, KLU, and CooperativeType tables with relationships

---
+++ backend/database/migrations/2025_04_08_065316_create_n_p_a_k_s_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('npaks', function (Blueprint $table) {
+            $table->id('notary_id');
+            $table->string('name', 50);
+            $table->string('address', 50);
+            $table->timestamps();
+            $table->unsignedBigInteger('provinceId'); // FK ke tabel provinsi
+
+            $table->foreign('provinceId')
+                ->references('province_id')
+                ->on('provinces') // pastikan kamu punya tabel ini
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('npaks');
+    }
+};
