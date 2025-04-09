52ccc81bba32d029cb14b78144e8076b82054f93
Emil Maulana <emilmaulana10gmail.com>
2025-04-09 09:53:40 +0700

    logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Api\CooperativeController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
