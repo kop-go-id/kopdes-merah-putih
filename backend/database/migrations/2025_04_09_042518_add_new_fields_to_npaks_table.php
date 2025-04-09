d9a924d18073fbbc6886bd390ecd00422931f09c
Emil Maulana <emilmaulana10@gmail.com>
2025-04-09 11:37:21 +0700

    Add new fields to npaks table: districtId, ahu_number, sk_number, certificate_training, email, primary_phone, secondary_phone, office_telephone

---
+++ backend/database/migrations/2025_04_09_042518_add_new_fields_to_npaks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('npaks', function (Blueprint $table) {
+            $table->char('districtId', 50)->nullable(false);
+            $table->char('ahu_number', 50)->nullable(false);
+            $table->char('sk_number', 50)->nullable(false);
+            $table->char('certificate_training', 50)->nullable(false);
+            $table->char('email', 50)->nullable(false);
+            $table->char('primary_phone', 50)->nullable(false);
+            $table->char('secondary_phone', 50)->nullable(false);
+            $table->char('office_telephone', 50)->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('npaks', function (Blueprint $table) {
+            $table->dropColumn([
+                'districtId',
+                'ahu_number',
+                'sk_number',
+                'certificate_training',
+                'email',
+                'primary_phone',
+                'secondary_phone',
+                'office_telephone',
+            ]);
+        });
+    }
+};
