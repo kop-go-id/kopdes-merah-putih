730d6d7d0225859182208c04dda81c56e69d45d4
Zain <masterzera111@gmail.com>
2025-04-09 16:07:20 +0700

    [FEAT] change npak by province and district code

--- backend/app/Http/Controllers/Api/NPAKController.php
+++ backend/app/Http/Controllers/Api/NPAKController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\District;
+use App\Models\Province;
 use App\Models\NPAK;
 
 class NPAKController extends Controller
@@ -17,18 +19,34 @@ class NPAKController extends Controller
         ], 201);
     }
 
-    public function getByDistrict($districtId)
+    public function getByDistrict($districtCode)
     {
-        $npaks = NPAK::where('districtId', $districtId)->with(['province', 'district'])->orderBy('name', 'asc')->get();
+        $district = District::where('code', $districtCode)->first();
+        if (!$district) {
+            return response()->json([
+                'message' => 'Invalid district',
+                'error' => 'Invalida district'
+            ], 500);
+        }
+
+        $npaks = NPAK::where('districtId', $district->id)->with(['province', 'district'])->orderBy('name', 'asc')->get();
         return response()->json([
             'message' => 'Success',
             'data' => $npaks,
         ], 201);
     }
 
-    public function getByProvince($provinceId)
+    public function getByProvince($provinceCode)
     {
-        $npaks = NPAK::where('provinceId', $provinceId)->with(['province', 'district'])->orderBy('name', 'asc')->get();
+        $province = Province::where('code', $provinceCode)->first();
+        if (!$province) {
+            return response()->json([
+                'message' => 'Invalid province',
+                'error' => 'Invalida province'
+            ], 500);
+        }
+
+        $npaks = NPAK::where('provinceId', $province->id)->with(['province', 'district'])->orderBy('name', 'asc')->get();
         return response()->json([
             'message' => 'Success',
             'data' => $npaks,
