6746ae447dd8b95dd8ad117a00ecc61f99db7d7c
Zain <masterzera111@gmail.com>
2025-04-11 16:03:18 +0700

    [FEAT] login

--- frontend/src/pages/masuk/index.js
+++ frontend/src/pages/masuk/index.js
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { login } from '@/services/auth';
+import { signIn } from 'next-auth/react';
 
 export default function LoginFormAntd() {
   const [form] = Form.useForm();
